765dc7a3a9734de5b61ed24aa14d7b91a5742c35
Hasnain17 <hasnaingakhar1998@gmail.com>
2024-05-27 17:31:06 +0500

    OPTIMIZE THE CODE:
      TODO:
      1: Image Button for Volume Up
      2: Image Button for Volume Down
      3: Image Button for Screen OFF
      4: Image Button for Power Menu

--- app/src/main/java/com/power/button/volume/keys/remap/utils/service/AccessibilityService.kt
+++ app/src/main/java/com/power/button/volume/keys/remap/utils/service/AccessibilityService.kt
@@ -25,8 +25,8 @@ class AccessibilityService : AccessibilityService() {
     }
 
     override fun onKeyEvent(event: KeyEvent): Boolean {
-        val actionTypeMinus=MyApp.prefs.pull<Int>("eventIDMinus")
-        val actionTypePlus=MyApp.prefs.pull<Int>("eventIDPlus")
+        val actionTypeMinus = MyApp.prefs.pull<Int>("eventIDMinus")
+        val actionTypePlus = MyApp.prefs.pull<Int>("eventIDPlus")
         /***
          * For Default Action   --> 0
          * For Turn Off Screen  --> 1
@@ -34,86 +34,41 @@ class AccessibilityService : AccessibilityService() {
          * For Do Nothing       --> 3
          * */
 
-        //For Volume Key Up
-        if (event.keyCode == KeyEvent.KEYCODE_VOLUME_UP && event.action == KeyEvent.ACTION_DOWN){
-            if (actionTypePlus!=0)
-            {
-                if (actionTypePlus==1){
-                    //For Turn OFF Screen
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)
-                    {
-                        performGlobalAction(GLOBAL_ACTION_LOCK_SCREEN);
-                    } else
-                    {
-                        // Provide an alternative or show a message
-                        performGlobalAction(GLOBAL_ACTION_POWER_DIALOG);
+        fun performAction(actionType: Int): Boolean {
+            return when (actionType) {
+                1 -> {
+                    // For Turn OFF Screen
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                        performGlobalAction(GLOBAL_ACTION_LOCK_SCREEN)
+                    } else {
+                        performGlobalAction(GLOBAL_ACTION_POWER_DIALOG)
                     }
-                    return true;
+                    true
                 }
-
-                if (actionTypePlus==2){
-                    //For Open Power Menu
-
-                    performGlobalAction(GLOBAL_ACTION_POWER_DIALOG);
-                    return true
-
+                2 -> {
+                    // For Open Power Menu
+                    performGlobalAction(GLOBAL_ACTION_POWER_DIALOG)
+                    true
                 }
-
-                if (actionTypePlus==3)
-                {
-                    //For Do Nothing
-                    return true
+                3 -> {
+                    // For Do Nothing
+                    true
                 }
+                else -> false
             }
         }
 
-
-
-        //For Volume Key Down
-        if (event.keyCode == KeyEvent.KEYCODE_VOLUME_DOWN && event.action == KeyEvent.ACTION_DOWN){
-            if (actionTypeMinus!=0)
-            {
-                    if (actionTypeMinus==1){
-                        //For Turn OFF Screen
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)
-                        {
-                            performGlobalAction(GLOBAL_ACTION_LOCK_SCREEN);
-                        } else
-                        {
-                            // Provide an alternative or show a message
-                            performGlobalAction(GLOBAL_ACTION_POWER_DIALOG);
-                        }
-                        return true;
-                    }
-
-                    if (actionTypeMinus==2){
-                        //For Open Power Menu
-
-                        performGlobalAction(GLOBAL_ACTION_POWER_DIALOG);
-                        return true
-
-                    }
-
-                    if (actionTypeMinus==3)
-                    {
-                        //For Do Nothing
-                        return true
-                    }
+        if (event.action == KeyEvent.ACTION_DOWN) {
+            return when (event.keyCode) {
+                KeyEvent.KEYCODE_VOLUME_UP -> {
+                    if (actionTypePlus != 0) performAction(actionTypePlus) else super.onKeyEvent(event)
+                }
+                KeyEvent.KEYCODE_VOLUME_DOWN -> {
+                    if (actionTypeMinus != 0) performAction(actionTypeMinus) else super.onKeyEvent(event)
+                }
+                else -> super.onKeyEvent(event)
             }
         }
-
-
         return super.onKeyEvent(event)
-    }
-
-
-    private fun showPowerMenu() {
-        try {
-            val keyEvent = KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_POWER)
-            val instrumentation = Instrumentation()
-            instrumentation.sendKeyDownUpSync(keyEvent.keyCode)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
     }
 }
